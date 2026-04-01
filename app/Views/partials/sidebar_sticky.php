e5e94caef4414a9bed4070c7b8417e255192835e
Artur Sabanadze <diakosmisi26@hotmail.com>
2026-04-01 05:00:26 +0200

    creating sidebar tool

---
+++ app/Views/partials/sidebar_sticky.php
@@ -0,0 +1,17 @@
+<div class="sb_wrapper">
+    <div class="sb_container">
+        <!-- scroll to the top button -->
+        <div class="sb_navigation_up"></div>
+            <!-- element container -->
+            <div class="sb_elements">
+                <!-- contact element -->
+                <div class="sb_item"></div>
+                <!-- calculator element -->
+                <div class="sb_item"></div>
+                <!-- whatsapp element -->
+                <div class="sb_item"></div>
+            </div>
+        <!-- scroll to the bottom button -->
+        <div class="sb_navigation_down"></div>
+    </div>
+</div>
