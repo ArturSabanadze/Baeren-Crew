3d0207fdeeba5319fa0c0971afc0995cb1217a64
Artur Sabanadze <diakosmisi26@hotmail.com>
2026-04-05 15:13:30 +0300

    hero container 3 cards

--- app/Views/partials/threeD_hero_cards.php
+++ app/Views/partials/threeD_hero_cards.php
@@ -1,41 +1,42 @@
-<div class="wrapper">
-    <div class="card blue">
-        <h2>Umzüge</h2>
-        <ul>
-            <li>✔ Privatumzüge</li>
-            <li>✔ Firmenumzüge</li>
-            <li>✔ Senioren</li>
-            <li>✔ Kleintransporte / Möbeltaxi </li>
-            <li>✔ Montage & Demontage Möbel </li>
-            <li>✔ Küche / Wandmontagen </li>
-            <li>✔ Ein- und Auspackservice </li>
-            <li>✔ Umzugskartons verschiedene größen und varianten </li>
-        </ul>
+<div class="hero-container-wrapper">
+    <div class="card-wrapper">
+        <div class="card blue">
+            <h2>Umzüge</h2>
+            <ul>
+                <li>✔ Privatumzüge</li>
+                <li>✔ Firmenumzüge</li>
+                <li>✔ Senioren</li>
+                <li>✔ Kleintransporte / Möbeltaxi </li>
+                <li>✔ Montage & Demontage Möbel </li>
+                <li>✔ Küche / Wandmontagen </li>
+                <li>✔ Ein- und Auspackservice </li>
+                <li>✔ Umzugskartons verschiedene größen und varianten </li>
+            </ul>
+        </div>
+        <div class="card gray">
+            <h2>Entrümpelungen & Haushaltsauflösungen </h2>
+            <ul>
+                <li>✔ Wohnungsentrümpelung</li>
+                <li>✔ Keller</li>
+                <li>✔ Garage</li>
+                <li>✔ Räumung stark belasteter Wohnungen </li>
+                <li>✔ Dachbodenentrümpelung </li>
+                <li>✔ Nachlass-Entrümpelung </li>
+                <li>✔ Büro- / Gewerbeentrümpelung </li>
+            </ul>
+        </div>
+        <div class="card green">
+            <h2>Objektbetreuung / Hausmeisterservice</h2>
+            <ul>
+                <li>✔ Regelmäßige Objektbetreuung</li>
+                <li>✔ Kleinreparaturen & Instandhaltung </li>
+                <li>✔ Renovierungsarbeiten </li>
+                <li>✔ Bodenverlegung (Laminat, Vinyl etc.) </li>
+                <li>✔ Gartenpflege & Außenanlagen </li>
+                <li>✔ Winterdienst </li>
+                <li>✔ Treppenhausreinigung </li>
+            </ul>
+        </div>
     </div>
-
-    <div class="card gray">
-        <h2>Entrümpelungen & Haushaltsauflösungen </h2>
-        <ul>
-            <li>✔ Wohnungsentrümpelung</li>
-            <li>✔ Keller / Garage</li>
-            <li>✔ Räumung stark belasteter Wohnungen </li>
-            <li>✔ Dachbodenentrümpelung </li>
-            <li>✔ Nachlass-Entrümpelung </li>
-            <li>✔ Büro- / Gewerbeentrümpelung </li>
-        </ul>
-    </div>
-
-    <div class="card green">
-        <h2>Objektbetreuung / Hausmeisterservice</h2>
-        <ul>
-            <li>✔ Regelmäßige Objektbetreuung</li>
-            <li>✔ Kleinreparaturen & Instandhaltung </li>
-            <li>✔ Renovierungsarbeiten </li>
-            <li>✔ Bodenverlegung (Laminat, Vinyl etc.) </li>
-            <li>✔ Gartenpflege & Außenanlagen </li>
-            <li>✔ Winterdienst </li>
-            <li>✔ Treppenhausreinigung </li>
-        </ul>
-    </div>
-
+    <h1> Ihr Auftrag – unsere Verantwortung.</h1>
 </div>
